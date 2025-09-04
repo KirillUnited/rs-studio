51aa5ccc24b559b7c1950abe56607a30eca981ef
dubov <Kirill.Dubov@enterra-inc.com>
2025-09-04 12:12:41 +0300

    feat(analytics): add google tag manager and analytics integration
    Enable Google Tag Manager and Google Analytics tracking by adding required dependencies and implementing the components in the layout. Uses environment variables for configuration.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,4 @@
-// import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 // import { Analytics } from '@vercel/analytics/react'
 // import { SpeedInsights } from '@vercel/speed-insights/next'
 import Root from '@/ui/Root'
@@ -15,33 +15,35 @@ import { HeroUIProvider } from '@heroui/react'
 import { Banner } from '@/components/banner'
 
 export default async function RootLayout({
-																					 children,
-																				 }: {
+	children,
+}: {
 	children: React.ReactNode
 }) {
 	return (
 		<Root>
-			{/* <GoogleTagManager gtmId="" /> */}
+			{/* ✅ GTM + GA4 */}
+			<GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
+			<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
 			<body className={cn('bg-background text-foreground antialiased flex flex-col min-h-screen dark',
 				fontSans.variable,
 			)}>
-			<HeroUIProvider>
-				<NuqsAdapter>
-					<SkipToContent />
-					{/*<Announcement />*/}
-					<Banner />
-					<Header />
-					<main id="main-content" role="main" className="flex-1">
-						{children}
-					</main>
-					<Footer />
+				<HeroUIProvider>
+					<NuqsAdapter>
+						<SkipToContent />
+						{/*<Announcement />*/}
+						<Banner />
+						<Header />
+						<main id="main-content" role="main" className="flex-1">
+							{children}
+						</main>
+						<Footer />
 
-					<VisualEditingControls />
-				</NuqsAdapter>
-			</HeroUIProvider>
+						<VisualEditingControls />
+					</NuqsAdapter>
+				</HeroUIProvider>
 
-			{/*<Analytics />*/}
-			{/*<SpeedInsights />*/}
+				{/*<Analytics />*/}
+				{/*<SpeedInsights />*/}
 			</body>
 		</Root>
 	)
