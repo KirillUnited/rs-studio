a6f62e5543c24bbed758c432fbba40c932f405d2
dubov <Kirill.Dubov@enterra-inc.com>
2025-09-21 13:27:14 +0300

    Refactor footer and social components layout
    Moved Social component from footer/index.tsx to footer/Navigation.tsx for improved layout and separation. Updated Social to display title and use a list for items. Minor style and structure adjustments in HeroSection and HeroSplit for consistency.

--- src/ui/Social.tsx
+++ src/ui/Social.tsx
@@ -13,6 +13,7 @@ import {
 } from 'react-icons/fa6'
 import { IoIosLink } from 'react-icons/io'
 import type { ComponentProps } from 'react'
+import { stegaClean } from 'next-sanity'
 
 export default async function Social({ className }: ComponentProps<'div'>) {
 	const { social } = await getSite()
@@ -20,32 +21,33 @@ export default async function Social({ className }: ComponentProps<'div'>) {
 	if (!social?.items?.length) return null
 
 	return (
-		<nav className={cn('group flex flex-wrap items-center', className)}>
-			{social.items.map((item, key) => {
-				switch (item._type) {
-					case 'link':
-						return (
-							<CTA
-								className="px-2 py-1 group-has-[a:hover]:opacity-50 hover:!opacity-100"
-								link={item}
-								key={key}
-							>
-								<Icon url={item.external} aria-label={item.label} />
-							</CTA>
-						)
-
-					default:
-						return null
-				}
-			})}
-		</nav>
+		<div className={cn('space-y-2 text-start', className)}>
+			{social.title && (
+				<div className="technical text-canvas/50 text-xs">
+					{stegaClean(social.title)}
+				</div>
+			)}
+			<ul className={cn('group flex flex-wrap items-center')}>
+				{social.items.map((item, key) => {
+					return (
+						<CTA
+							className="px-2 py-1 group-has-[a:hover]:opacity-50 hover:!opacity-100"
+							link={item}
+							key={key}
+						>
+							<Icon url={item.external} aria-label={item.label} />
+						</CTA>
+					)
+				})}
+			</ul>
+		</div>
 	)
 }
 
 function Icon({
-	url,
-	...props
-}: { url?: string } & React.ComponentProps<'svg'>) {
+								url,
+								...props
+							}: { url?: string } & React.ComponentProps<'svg'>) {
 	if (!url) return null
 
 	return url?.includes('bsky.app') ? (
